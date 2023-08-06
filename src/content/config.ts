33e54a5cec10b20a3d47ba6548332c68b0e6aaec
Gagaoreo <cole1.marino@gmail.com>
2023-08-06 19:00:46 -0400

    Config file for collections to be used in maps and such!

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+export const collections = {
+	projects: defineCollection({
+		schema: z.object({
+			current: z.boolean(),
+			title: z.string(),
+			description: z.string(),
+			publishDate: z.coerce.date(),
+			tags: z.array(z.string()),
+			img: z.string(),
+			img_alt: z.string().optional(),
+		}),
+	}),
+};
