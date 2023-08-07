6a6e686cbe3cd9dae15b75fd8e0e38c598bc2a1d
Gagaoreo <cole1.marino@gmail.com>
2023-08-06 23:08:52 -0400

    still kinda broken, but created the .mdx info file for the website project.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,20 @@
 import { defineCollection, z } from 'astro:content';
 
+const projects = defineCollection({
+	schema: z.object({
+		current: z.boolean(),
+		title: z.string(),
+		description: z.string(),
+		publishDate: z.coerce.date(),
+		tags: z.array(z.string()),
+		languages: z.array(z.string()),
+		img: z.string(),
+		img_alt: z.string().optional(),
+
+		data: z.string(),
+	})
+});
+
 export const collections = {
-	projects: defineCollection({
-		schema: z.object({
-			current: z.boolean(),
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			img: z.string(),
-			img_alt: z.string().optional(),
-		}),
-	}),
+	'projects' : projects,
 };
