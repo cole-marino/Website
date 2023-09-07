bdb4c85fc7c4de8d314d992273e7864882560e5f
Gagaoreo <cole1.marino@gmail.com>
2023-09-07 12:24:32 -0400

    Added font configurations for tailwindCSS.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,44 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+
+		fontSize: {
+			'xs': '.75rem',
+			'sm': '.875rem',
+			'tiny': '.875rem',
+			'base': '1rem',
+			'lg': '1.125rem',
+			'xl': '1.25rem',
+			'2xl': '1.5rem',
+			'3xl': '1.875rem',
+			'4xl': '2.25rem',
+			'5xl': '3rem',
+			'6xl': '4rem',
+			'7xl': '5rem',
+
+			// header font sizes
+			'h-xm': '35px',		// extra small screen
+			'h-sm': '52px',		// small screen
+			'h-md': '75px',		// medium screen
+			'h-lg': '100px',	// large screen
+
+			// title text font sizes
+			'tt-xm': '30px',
+			'tt-sm': '28px',
+			'tt-md': '38px',
+			'tt-lg': '42px',
+
+			// main text font sizes
+			't-xm': '15px',
+			't-sm': '20px',
+			't-md': '24px',
+			't-lg': '28px',		// baseline on my display
+		},
+
 		extend: {},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
 	],
+
 }
