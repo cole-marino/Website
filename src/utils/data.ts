b37df35c6363eabf3a964f062cd2ad090976cd62
Gagaoreo <cole1.marino@gmail.com>
2023-09-25 13:51:45 -0400

    Created folder for supporting utilities.

---
+++ src/utils/data.ts
@@ -0,0 +1,50 @@
+const csSkills=[
+
+    {
+        title: 'AstroHTML',
+        url: 'https://astro.build/',
+        img: './images/assets/astro.png',
+        img_alt: './images/assets/astro.png',
+
+    },
+
+    {
+        title: 'TailwindCSS',
+        url: 'https://tailwindcss.com/',
+        img: './images/assets/tailwind.svg',
+        img_alt: './images/assets/tailwind.svg',
+
+    },
+
+    {
+        title: 'Java',
+        url: 'https://www.java.com/en/',
+        img: './images/assets/java.svg',
+        img_alt: './images/assets/java.svg',
+
+    },
+    
+    {
+        title: 'Python',
+        url: 'https://www.python.org/',
+        img: './images/assets/python.png',
+        img_alt: './images/assets/python.png',
+
+    },
+    
+    {
+        title: 'C',
+        url: 'https://devdocs.io/c/',
+        img: './images/assets/C.svg',
+        img_alt: './images/assets/C.svg',
+
+    },
+
+    {
+        title: 'C++',
+        url: 'https://cplusplus.com/',
+        img: './images/assets/C++.png',
+        img_alt: './images/assets/C++.png',
+
+    },
+]
